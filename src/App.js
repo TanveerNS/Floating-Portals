7769dc515a95b57d5e560d0e7d79ba3f5e222b3b
tanveerns <tanveeralam229@gmail.com>
2022-11-19 23:05:51 +0530

    car show

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, {useState} from 'react'
 import './App.css';
+import CarShow from './CarShow';
 import Portals from './Portals';
 
 function App() {
   const [currentScene, setCurrentScene] = useState('')
   switch(currentScene){
     case 'portals': return <Portals />
+    case 'show': return <CarShow />
     case 'racing': return <h1>Racing</h1>
     case 'default' : <h1>Hello World!</h1>
   }
   return (
     <div className="App">
       <button onClick={()=> setCurrentScene('portals')}>Portals</button>
+      <button onClick={()=> setCurrentScene('show')}>Car Show</button>
       <button onClick={()=> setCurrentScene('racing')}>Racing</button>
     </div>
   );
